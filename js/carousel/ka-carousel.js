f2dcbc0c23bdfb24cfc61eabe04859acf1b16734
Mason Payne <gomas.bmw@gmail.com>
2015-10-18 22:25:39 -0600

    Add more images and use the carousel

--- js/carousel/ka-carousel.js
+++ js/carousel/ka-carousel.js
@@ -10,17 +10,21 @@ angular.module('app').directive('kaCarousel', [function() {
         scope.images = [];
       }
       scope.images = [
-        'IMG_0944-Edit-small.png',
-        'IMG_0956-Edit-small.png',
-        'IMG_0958-Edit-small.png',
-        'IMG_0959-Edit-small.png',
-        'IMG_0961-Edit-small.png',
-        'IMG_0962-Edit-small.png',
-        'IMG_0963-Edit-small.png',
-        'IMG_0964-Edit-small.png',
-        'IMG_0966-Edit-small.png',
-        'IMG_0967-Edit-small.png',
-        'IMG_0970-Edit-small.png',
+        'IMG_0944-Edit-small-black.png',
+        'IMG_0008.jpg',
+        'IMG_0009.jpg',
+        'IMG_0010.jpg',
+        'IMG_0011.jpg',
+        // 'IMG_0956-Edit-small.png',
+        // 'IMG_0958-Edit-small.png',
+        // 'IMG_0959-Edit-small.png',
+        // 'IMG_0961-Edit-small.png',
+        // 'IMG_0962-Edit-small.png',
+        // 'IMG_0963-Edit-small.png',
+        // 'IMG_0964-Edit-small.png',
+        // 'IMG_0966-Edit-small.png',
+        // 'IMG_0967-Edit-small.png',
+        // 'IMG_0970-Edit-small.png',
       ];
     }
   };
